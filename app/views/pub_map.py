a124a0b36623f404c6beb28b5eb4d55de2a0147b
Andy Carter <andyjcarter77@gmail.com>
2023-03-15 08:27:48 +0000

    transferred js functions

--- app/views/pub_map.py
+++ app/views/pub_map.py
@@ -1,20 +1,27 @@
+import json
+import pandas as pd
 from flask import render_template
 from app import app
+from config import Configurations
+from functions.functions import Functions
+
+config = Configurations().get_config()
+function = Functions()
 
 
 @app.route("/pub/map/stations")
 def map_stations():
     print('/pub/map/stations')
-    df_stations = get_stations()
-    df_pubs_reviews = get_pubs_reviews()
-    df_reviewed_only = df_pubs_reviews.loc[df_pubs_reviews['colour'] != constants.get("Colours", "NEW")]
+    df_stations = function.get_stations()
+    df_pubs_reviews = function.get_pubs_reviews()
+    df_reviewed_only = df_pubs_reviews.loc[df_pubs_reviews['colour'] != config['colour_new']]
     df_truncated = df_reviewed_only[['name', 'station_identity']]
     df_result = df_truncated.groupby(['station_identity'], as_index=False).count()
     df_result_latlng = pd.merge(df_result, df_stations, how='left', on='station_identity')
     df_sorted = df_result_latlng.rename(columns={'name': 'count'}).astype(str)
-    df_sorted['colour'] = constants.get("Colours", "REVIEW")
-    stations_json = df_to_dict(df_sorted)
-    pubs_reviews_json = df_to_dict(df_pubs_reviews)
+    df_sorted['colour'] = config['colour_review']
+    stations_json = function.df_to_dict(df_sorted)
+    pubs_reviews_json = function.df_to_dict(df_pubs_reviews)
     view = "stations"
     return render_template('pub_map.html', google_key=config['google_key'], stations=stations_json,
                            pubs_reviews=pubs_reviews_json, icon_hole=False,
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from configparser import ConfigParser
 
 
@@ -8,4 +9,35 @@ class Configurations:
         directory_path = os.getcwd()
         constants = ConfigParser()
         constants.read(directory_path + "/constants.ini")
-        return constants
+        debug = True
+        config = dict()
+
+        config['aws_key_pub'] = constants.get("Aws_key", "PUB")
+        config['aws_key_review'] = constants.get("Aws_key", "REVIEW")
+        config['aws_key_station'] = constants.get("Aws_key", "STATION")
+
+        config['aws_prefix_pub'] = constants.get("Aws_prefix", "PUB")
+        config['aws_prefix_review'] = constants.get("Aws_prefix", "REVIEW")
+        config['aws_prefix_station'] = constants.get("Aws_prefix", "STATION")
+
+        config['columns_pub'] = constants.get("Columns", "PUB")
+        config['columns_review'] = constants.get("Columns", "REVIEW")
+        config['columns_station'] = constants.get("Columns", "STATION")
+        config['columns_score'] = constants.get("Columns", "SCORE")
+
+        config['colour_new'] = constants.get("Colours", "NEW")
+        config['colour_review'] = constants.get("Colours", "REVIEW")
+
+        if debug:
+            with open(os.getcwd() + '/config.json') as file:  # Opening JSON file
+                config_file = json.load(file)  # returns JSON object as a dictionary
+                config['google_key'] = config_file['configs']['local_key']
+                config['access_id'] = config_file['configs']['access_id']
+                config['access_key'] = config_file['configs']['access_key']
+                config['bucket_name'] = config_file['configs']['bucket_name']
+        else:
+            config['google_key'] = os.getenv("HEROKU_GOOGLE_API")
+            config['access_id'] = os.environ.get("ACCESS_ID")
+            config['access_key'] = os.environ.get("ACCESS_KEY")
+            config['bucket_name'] = os.environ.get("BUCKET_NAME")
+        return config
